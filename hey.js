bfc0bada86be2f8620727b3ad4805b7f21bc2cc9
Jawad Hassan <itsjawadhasan@gmail.com>
2024-07-03 17:13:53 +0500

    change the commit in console log

--- hey.js
+++ hey.js
@@ -1,6 +1,6 @@
 const clickElement = document.querySelector("h2");
 clickElement.addEventListener('click', function a() {
     
-    console.log('Element clicked!')
+    console.log('Element is clicked!')
     alert('hey jawad')
 });
